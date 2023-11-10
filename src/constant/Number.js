f9a208559fbd8952d5d84181b4b26d98d77acc98
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-10 13:53:08 +0900

    Feat(Number): 기능에 필요한 매직 넘버 생성

---
+++ src/constant/Number.js
@@ -0,0 +1,14 @@
+const NUMBER = Object.freeze({
+  dateDiscount: 2_023,
+  dDayDefaultDiscount: 1_000,
+  dDayPlusDiscount: 100,
+  specialDiscount: 1_000,
+  giveawayStandard: 120_000,
+  benefitStandard: 10_000,
+  menusMaxCount: 20,
+  starBenefit: 5_000,
+  treeBenefit: 10_000,
+  santaBenefit: 20_000,
+});
+
+export default NUMBER;
