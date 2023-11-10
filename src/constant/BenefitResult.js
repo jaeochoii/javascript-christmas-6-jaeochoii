ba960a7bfc753f5418389224b1ea54108a75f2ff
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-11 00:30:19 +0900

    Refactor(Message): 혜택 기호 분리

--- src/constant/BenefitResult.js
+++ src/constant/BenefitResult.js
@@ -1,7 +1,7 @@
 const BENEFIT_RESULT = Object.freeze([
-  "크리스마스 디데이 할인: -",
-  "평일 할인: -",
-  "특별 할인: -",
-  "증정 이벤트: -",
+  "크리스마스 디데이 할인:",
+  "평일 할인:",
+  "특별 할인:",
+  "증정 이벤트:",
 ]);
 export default BENEFIT_RESULT;
--- src/constant/Message.js
+++ src/constant/Message.js
@@ -2,8 +2,8 @@ const MESSAGE = Object.freeze({
   intro: "안녕히세요! 우테코 식당 12월 이벤트 플래너입니다.",
   date: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
   menu: "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n",
-  previewPrefix: "12월 ",
-  previewSuffix: "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+  previewPrefix: "12월",
+  previewSuffix: "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
   order: "<주문 메뉴>",
   beforeDiscount: "<할인 전 총주문 금액>",
   afterDiscount: "<할인 후 예상 결제 금액>",
@@ -16,6 +16,8 @@ const MESSAGE = Object.freeze({
   starBadge: "별",
   treeBadge: "트리",
   santaBadge: "산타",
+  champagne: "샴페인",
+  benefit: "-",
 });
 
 export default MESSAGE;
