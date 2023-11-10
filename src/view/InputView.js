9749ea13be7ab5d0663d1703deea16cf98d02b7b
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-10 23:27:04 +0900

    Feat(InputView): 입력 관련 처리 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,21 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGE from "../constant/Message.js";
+import Validator from "../utils/Validator.js";
+
+const InputView = {
+  async readDate() {
+    const input = await Console.readLineAsync(MESSAGE.date);
+    Validator.inputDate(input);
+
+    return input;
+  },
+
+  async readMenus() {
+    const input = await Console.readLineAsync(MESSAGE.menu);
+    Validator.inputOrder(input);
+
+    return input;
+  },
+};
+
+export default InputView;
