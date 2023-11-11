9c3a3781a2742347e5577b607519f346f93d04f7
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-11 20:32:15 +0900

    Test(OrderedMenusClassTest): 입력받은 메뉴의 단위 기능 테스트 구현

---
+++ __tests__/OrderedMenusClassTest.js
@@ -0,0 +1,28 @@
+import OrderedMenus from "../src/domain/OrderedMenus.js";
+
+describe("OrderedMenus 클래스 단위 기능 테스트", () => {
+  test("입력한 메뉴가 하나의 객체로 분리 되는지 확인", () => {
+    const inputMenus = "티본스테이크-1,양송이수프-2,제로콜라-3";
+    const answer = {
+      티본스테이크: 1,
+      양송이수프: 2,
+      제로콜라: 3,
+    };
+
+    expect(new OrderedMenus(inputMenus).getOrderedMenus()).toEqual(answer);
+  });
+
+  test("입력한 메뉴에 음료만 입력되었는지 확인", () => {
+    const inputMenus = "제로콜라-3";
+    expect(() => {
+      new OrderedMenus(inputMenus);
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력한 메뉴의 개수가 총합 20이 넘었는지 확인", () => {
+    const inputMenus = "티본스테이크-10,양송이수프-10,제로콜라-3";
+    expect(() => {
+      new OrderedMenus(inputMenus);
+    }).toThrow("[ERROR]");
+  });
+});
