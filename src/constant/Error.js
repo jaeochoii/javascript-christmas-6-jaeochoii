0f660964e9d77af3ada9427fc68154577eb27e4f
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-12 17:59:18 +0900

    Fix(Error): 에러 메시지 출력 관련 문자열 오류 해결

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -1,6 +1,6 @@
 const ERROR = Object.freeze({
   date: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
-  order: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.",
+  order: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
   orderDrink: "[ERROR] 음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요",
   orderLimit:
     "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요",
