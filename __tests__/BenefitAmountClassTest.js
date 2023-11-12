4a0157af0a09aa833b940420f50fd57971e0d229
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-12 13:45:27 +0900

    Test(BenefitAmountClassTest): 25일 이후 날짜가 입력될 시 디데이 이벤트 적용X 테스트 추가

--- __tests__/BenefitAmountClassTest.js
+++ __tests__/BenefitAmountClassTest.js
@@ -1,7 +1,7 @@
 import BenefitAmount from "../src/domain/BenefitAmount.js";
 
 describe("BenefitAmount 클래스 단위 기능 테스트", () => {
-  test("Event 클래스로부터 얻은 혜택 내역을 기반으로 금액으로 환산 되는지 확인(주말)", () => {
+  test("Event 클래스로부터 얻은 혜택 내역을 기반, 금액으로 환산 되는지 확인(주말)", () => {
     const menus = { 티본스테이크: 2, 초코케이크: 1, 제로콜라: 1 };
     const benefitList = [15, 1, 0, 0, 0];
     const totalAmount = 123000;
@@ -12,7 +12,7 @@ describe("BenefitAmount 클래스 단위 기능 테스트", () => {
     ).toEqual(answer);
   });
 
-  test("Event 클래스로부터 얻은 혜택 내역을 기반으로 금액으로 환산 되는지 확인(평일, 특별할인 X)", () => {
+  test("Event 클래스로부터 얻은 혜택 내역을 기반, 금액으로 환산 되는지 확인(평일, 특별할인 X)", () => {
     const menus = { 티본스테이크: 2, 초코케이크: 1, 제로콜라: 1 };
     const benefitList = [13, 0, 1, 0, 0];
     const totalAmount = 123000;
@@ -23,7 +23,7 @@ describe("BenefitAmount 클래스 단위 기능 테스트", () => {
     ).toEqual(answer);
   });
 
-  test("Event 클래스로부터 얻은 혜택 내역을 기반으로 금액으로 환산 되는지 확인(평일, 특별할인 O)", () => {
+  test("Event 클래스로부터 얻은 혜택 내역을 기반, 금액으로 환산 되는지 확인(평일, 특별할인 O)", () => {
     const menus = { 티본스테이크: 2, 초코케이크: 1, 제로콜라: 1 };
     const benefitList = [13, 0, 1, 1, 0];
     const totalAmount = 123000;
@@ -34,11 +34,22 @@ describe("BenefitAmount 클래스 단위 기능 테스트", () => {
     ).toEqual(answer);
   });
 
-  test("Event 클래스로부터 얻은 혜택 내역을 기반으로 금액으로 환산 되는지 확인(12만원 미만 결제)", () => {
+  test("Event 클래스로부터 얻은 혜택 내역을 기반, 금액으로 환산 되는지 확인(12만원 미만 결제)", () => {
     const menus = { 티본스테이크: 2, 초코케이크: 1, 제로콜라: 1 };
     const benefitList = [13, 0, 1, 1, 0];
     const totalAmount = 23000;
     const answer = [2200, 0, 2023, 1000, 0];
+
+    expect(
+      new BenefitAmount(menus, benefitList, totalAmount).getBenefitList()
+    ).toEqual(answer);
+  });
+
+  test("Event 클래스로부터 얻은 혜택 내역을 기반, 금액으로 환산 되는지 확인(날짜 25일 이후) ", () => {
+    const menus = { 티본스테이크: 2, 초코케이크: 1, 제로콜라: 1 };
+    const benefitList = [27, 0, 1, 1, 0];
+    const totalAmount = 123000;
+    const answer = [0, 0, 2023, 1000, 25000];
 
     expect(
       new BenefitAmount(menus, benefitList, totalAmount).getBenefitList()
