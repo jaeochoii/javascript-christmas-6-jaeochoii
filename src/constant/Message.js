aa74c66990586553f6fbbc7712961411d248569c
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-13 20:11:14 +0900

    Style(Message): 상수화한 문자열 선언 가독성 좋게 수정

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -2,8 +2,7 @@ const MESSAGE = Object.freeze({
   intro: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
   date: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
   menu: "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n",
-  previewPrefix: "12월",
-  previewSuffix: "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
+
   order: "<주문 메뉴>",
   beforeDiscount: "<할인 전 총주문 금액>",
   afterDiscount: "<할인 후 예상 결제 금액>",
@@ -11,12 +10,17 @@ const MESSAGE = Object.freeze({
   benefitList: "<혜택 내역>",
   benefitAccount: "<총혜택 금액>",
   eventBadge: "<12월 이벤트 배지>",
-  countSuffix: "개",
-  priceSuffix: "원",
+
+  previewPrefix: "12월",
+  previewSuffix: "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
+
   starBadge: "별",
   treeBadge: "트리",
   santaBadge: "산타",
   champagne: "샴페인",
+
+  countSuffix: "개",
+  priceSuffix: "원",
   benefit: "-",
   none: "없음",
   line: "\n",
