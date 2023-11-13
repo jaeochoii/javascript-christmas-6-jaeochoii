678035a354b1e9f931bb16edf3e18f3caea7fb3c
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-13 15:54:25 +0900

    Style(Number): 매직 넘버 가독성있게 분리화 작업

--- src/constant/Number.js
+++ src/constant/Number.js
@@ -1,23 +1,29 @@
 const NUMBER = Object.freeze({
   menuCountLimit: 1,
+  menusMaxCount: 20,
+
+  giveawayStandard: 120_000,
+  benefitStandard: 10_000,
+  champagne: 25_000,
+
   minDate: 1,
   maxDate: 31,
   dateLength: 7,
+  christmas: 25,
+
+  friRemainder: 1,
+  satRemainder: 2,
+  specialRemainder: 3,
+
   dateDiscount: 2_023,
   dDayDefaultDiscount: 1_000,
   dDayPlusDiscount: 100,
   specialDiscount: 1_000,
-  giveawayStandard: 120_000,
-  benefitStandard: 10_000,
-  menusMaxCount: 20,
+
   starBenefit: 5_000,
   treeBenefit: 10_000,
   santaBenefit: 20_000,
-  friRemainder: 1,
-  satRemainder: 2,
-  specialRemainder: 3,
-  christmas: 25,
-  champagne: 25_000,
+
   benefitListLength: 5,
   dDayEventIndex: 0,
   weekendEventIndex: 1,
