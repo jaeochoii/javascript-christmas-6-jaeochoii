022d964bd67783706856fb214d18b5e6b2b8742a
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-15 19:53:13 +0900

    Test(TotalAmountClassTest): 총 금액을 이용한 증정 메뉴 개수 확인하는 테스트 확인

--- __tests__/TotalAmountClassTest.js
+++ __tests__/TotalAmountClassTest.js
@@ -10,4 +10,14 @@ describe("TotalAmount 클래스 단위 기능 테스트", () => {
     const answer = 76_000;
     expect(new TotalAmount(menus).getTotalAmount()).toEqual(answer);
   });
+
+  test("입력받은 메뉴의 총 금액을 이용하여 증정 메뉴 개수 확인", () => {
+    const menus = {
+      티본스테이크: 1,
+      양송이수프: 2,
+      제로콜라: 3,
+    };
+    const answer = 3;
+    expect(new TotalAmount(menus).getGiveawayCount()).toEqual(answer);
+  });
 });
